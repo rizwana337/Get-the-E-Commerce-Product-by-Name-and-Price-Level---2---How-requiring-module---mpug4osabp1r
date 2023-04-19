bbd61169db67b70b7da22c07f3778f766018c748
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-19 16:56:12 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,38 @@ const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 app.use(express.json());
 
 // GET endpoint for sending the products to client by id
-//// Endpoint - /api/v1/products/:id
+app.get("/api/v1/products/:id", (req, res) => {
+  let { id } = req.params;
+  id *= 1;
+  const product = products.find((product) => product.id === id);
+  if (!product) {
+    return res.status(404).send({
+      status: "failed",
+      message: "Product not found!",
+    });
+  } else {
+    res.status(200).send({
+      status: "success",
+      message: "Product fetched successfully",
+      data: {
+        product,
+      },
+    });
+  }
+});
 
 module.exports = app;
+// const fs = require("fs");
+// const express = require("express");
+// const app = express();
+
+// // Importing products from products.json file
+// const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
+
+// //Middlewares
+// app.use(express.json());
+
+// // GET endpoint for sending the products to client by id
+// //// Endpoint - /api/v1/products/:id
+
+// module.exports = app;
